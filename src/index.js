9763fa0b2b8206077bb9245b9d55cdc68fb5fba5
Aleksandr <mscw81@gmail.com>
2022-08-12 16:18:58 +0300

    feat: modified js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+    for (let elem of bracketsConfig) 
+    {
+      array.push(elem.join(''))
+    }
+    
+    for (let i = 0; i < array.length; i++) 
+    {
+      if (str.includes(array[i])) 
+      {
+        str = str.replace(array[i], '');
+        i =- 1
+      }
+    }
+   return str.length === 0;
 }
+
